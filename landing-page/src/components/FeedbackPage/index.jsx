1d19e71b8a91c58df2e922aa4069e4994a735f0b
spanki22052 <useful22052@gmail.com>
2020-07-11 20:47:24 +0300

    feedback is ready

--- landing-page/src/components/FeedbackPage/index.jsx
+++ landing-page/src/components/FeedbackPage/index.jsx
@@ -6,6 +6,41 @@ const FeedbackPage = () => {
   const [numberInput, setNumber] = useState("");
   const [nameInput, setName] = useState("");
   const [worryInput, setWorry] = useState("");
+  const [feedbacksObject, setObject] = useState({});
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("feedbacks")
+      .doc("feedbacksList")
+      .get()
+      .then(async (info) => {
+        setObject(info.data().feedbacks);
+      });
+  }, []);
+
+  const sendFeedbackToDb = () => {
+    let number = {
+      number: numberInput,
+      name: nameInput,
+      worry: worryInput,
+    };
+
+    let newObject = { ...feedbacksObject };
+    newObject[numberInput] = number;
+
+    numberInput.length > 0 &&
+      nameInput.length > 0 &&
+      worryInput.length > 0 &&
+      setObject(newObject);
+    firebase.firestore().collection("feedbacks").doc("feedbacksList").set({
+      feedbacks: newObject,
+    });
+
+    setName('')
+    setNumber('')
+    setWorry('')
+  };
 
   return (
     <div className="feedback-page">
@@ -37,7 +72,7 @@ const FeedbackPage = () => {
             />
           </div>
           <div className="butt-block">
-            <button>Отправить</button>
+            <button onClick={sendFeedbackToDb}>Отправить</button>
           </div>
         </div>
       </div>
