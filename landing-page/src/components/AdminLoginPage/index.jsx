3b94b6a3d93bb6e20fc65cf401b98d9f92e2eb84
spanki22052 <useful22052@gmail.com>
2020-07-12 20:13:39 +0300

    adminpanel scss added

--- landing-page/src/components/AdminLoginPage/index.jsx
+++ landing-page/src/components/AdminLoginPage/index.jsx
@@ -4,7 +4,18 @@ import "./login.scss";
 const AdminLoginPage = () => {
   return (
     <div className="login-page">
-      <h1>Welcome to admin login page</h1>
+        <div className="login-block">
+            <h1>Вход в админ панель</h1>
+            <div className="input-block">
+                <p>Почта:</p>
+                <input type="text"/>
+            </div>
+            <div className="input-block">
+                <p>Пароль:</p>
+                <input type="text"/>
+            </div>
+
+        </div>
     </div>
   );
 };
