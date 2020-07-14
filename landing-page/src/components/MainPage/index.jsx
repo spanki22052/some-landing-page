835025feecb8105ec8e3f3285d7cba67a745141f
spanki22052 <useful22052@gmail.com>
2020-07-14 18:16:40 +0300

    add photos and repaired some problems with navbar

--- landing-page/src/components/MainPage/index.jsx
+++ landing-page/src/components/MainPage/index.jsx
@@ -18,12 +18,27 @@ const MainPage = () => {
         }}
       >
         <div className="white-block">
-          <img src="/images/doctor.jpeg" alt="doctorphoto"/>
+          <img src="/images/doctor.jpeg" alt="doctorphoto" />
           <h1>
             Я Джеймс - являюсь лучшим хирургом Махачкалы. Опыт работы - 10 лет.
             Вы можете довериться мне!
           </h1>
         </div>
+      </div>
+
+      <div className="minbor-block">
+        <a href="https://www.who.int/ru">
+          <img className="WR" src="/images/world.jpg" alt="gerb" />
+        </a>
+        <a href="https://minzdrav.gov.ru/">
+          <img className="RF" src="/images/gerb_minzdrava.png" alt="gerb" />
+        </a>
+        <a href="https://www.rospotrebnadzor.ru/">
+          <img className="RS" src="/images/rospotreb.png" alt="gerb" />
+        </a>
+        <a href="https://www.rospotrebnadzor.ru/">
+          <img className="DB" src="/images/dubaiD.png" alt="gerb" />
+        </a>
       </div>
     </div>
   );
--- landing-page/src/components/NavbarComponent/index.jsx
+++ landing-page/src/components/NavbarComponent/index.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import $ from "jquery";
 
 const NavbarComponent = () => {
+  var [photoLink, changeLink] = useState("/images/icon.png");
   useEffect(() => {
     $(".navTrigger").click(function () {
       $(this).toggleClass("active");
@@ -14,19 +15,54 @@ const NavbarComponent = () => {
     $(window).scroll(function () {
       if ($(document).scrollTop() > 50) {
         $(".nav").addClass("affix");
+        changeLink("/images/icon.png");
         console.log("OK");
       } else {
+        changeLink("/images/icon.png");
         $(".nav").removeClass("affix");
       }
     });
   }, []);
 
   return (
-    <div className="navbar-component" style={{ zIndex: '5' }}>
+    <div className="navbar-component" style={{ zIndex: "5" }}>
       <nav className="nav">
-        <div className="container">
+        <div
+          className="container"
+          style={{
+            width: "100% !important",
+            marginLeft: "5%",
+            marginRight: "0",
+          }}
+        >
           <div className="logo">
-            <a href="/">Джеймс</a>
+            <a href="/">
+              <img src={photoLink} alt="site-icon" style={{ float: "left" }} />
+              <div className="txt-blck">
+                <p
+                  style={{
+                    fontFamily: "Montserrat",
+                    fontWeight: "800",
+                    fontSize: "0.7em",
+                    lineHeight: "30px",
+                    marginTop: "10px",
+                  }}
+                >
+                  Doctor
+                </p>
+                <p
+                  style={{
+                    fontFamily: "Montserrat",
+                    fontWeight: "800",
+                    fontSize: "0.7em",
+                    lineHeight: "5px",
+                    marginLeft: '15px',
+                  }}
+                >
+                  Daudov
+                </p>
+              </div>
+            </a>
           </div>
           <div id="mainListDiv" className="main_list">
             <ul className="navlinks">
